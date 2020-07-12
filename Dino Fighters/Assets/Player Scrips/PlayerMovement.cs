6fa32f45aff11f59e9d7efcf462a1de4d51c684c
javis_c <jr.cuarajavier@gmail.com>
2020-07-11 23:09:44 -0700

    Current Camera and player scripts
    Vision is tied to cursor moments, and player movement is in the works.

---
+++ Dino Fighters/Assets/Player Scrips/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    // Default speed, it can be changed
+    public float speed = 12f;
+    public float gravity = -9.8f;
+    Vector3 velocity;
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 movement = transform.right * x  + transform.forward * z;
+
+        // DeltaTime bases movements that are not dependent on fps
+        controller.Move(movement * speed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+}
