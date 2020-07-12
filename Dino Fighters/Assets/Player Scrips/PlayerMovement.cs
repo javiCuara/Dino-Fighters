c129094a6428fcbbf621ed0dc2c34c54e263761d
javis_c <jr.cuarajavier@gmail.com>
2020-07-11 23:33:18 -0700

    made some updates to player movement

--- Dino Fighters/Assets/Player Scrips/PlayerMovement.cs
+++ Dino Fighters/Assets/Player Scrips/PlayerMovement.cs
@@ -10,16 +10,36 @@ public class PlayerMovement : MonoBehaviour
     // Default speed, it can be changed
     public float speed = 12f;
     public float gravity = -9.8f;
+    public float groundDistance = 0.4f;
+    public float jumpH = 3f;
+
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
     Vector3 velocity;
+    bool isGrounded;
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 movement = transform.right * x  + transform.forward * z;
 
         // DeltaTime bases movements that are not dependent on fps
         controller.Move(movement * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpH * -2f * gravity);
+        }
+
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
 
